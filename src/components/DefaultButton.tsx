2f60fb84d6ce175ac966f99565187213ecb626cf
999MeshDi666 <havkpzz@gmail.com>
2023-09-02 14:50:23 +0600

    Create components for todo card and button

---
+++ src/components/DefaultButton.tsx
@@ -0,0 +1,26 @@
+import {
+    Button,
+    View,
+} from 'react-native';
+import { ColorValue, GestureResponderEvent } from 'react-native';
+
+type DefaultButtonProps = {
+    disabled?: boolean | undefined,
+    color?: ColorValue | undefined,
+    title: string;
+    onPress?: ((event: GestureResponderEvent) => void) | undefined;
+}
+
+const DefaultButton = ({disabled, color, title, onPress}:DefaultButtonProps) =>{
+    return(
+        <View style={{marginTop: 8}}>
+            <Button 
+                disabled={disabled}
+                color={color} 
+                title={title} 
+                onPress={onPress}
+            />
+        </View>
+    )
+}
+export default DefaultButton;
